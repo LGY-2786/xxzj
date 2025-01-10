b885331f6435b5beec31e003bcdca58b3b5cd699
LGY <lgyxxzj@gmail.com>
2025-01-10 15:43:51 +0800

    修复 Hello.java 中的用户输入功能，确保正确读取用户输入

--- Hello.java
+++ Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
+        nextLine();
         
         greet(name);
         
