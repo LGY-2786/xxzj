6d904e07dd780c85e5bc558e14ab77225551fed1
LGY <lgyxxzj@gmail.com>
2025-01-10 15:43:39 +0800

    修改 Hello.java，添加用户输入功能并实现个性化问候

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        
+        greet(name);
+        
+        scanner.close();
+    }
+    
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
